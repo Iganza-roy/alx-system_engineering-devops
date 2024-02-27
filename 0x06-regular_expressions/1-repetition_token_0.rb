7cde15f8126f2e5f42617b6ff94ab39a53837579
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-27 09:17:03 -0800

    new

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-#accepts one argument and pass it to a regular expression matching method
+# accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/hbt*n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
