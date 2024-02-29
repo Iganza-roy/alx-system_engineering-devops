9dda74d641b51c4da6b5a064a55ab1e4ed8c757a
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-29 00:05:56 -0800

    new

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
-# 
+# bash script that outputs: [SENDER],[RECEIVER],[FLAGS]
+# The sender phone number or name (including country code if present)
+# The receiver phone number or name (including country code if present)
+# The flags that were used
 
-puts ARGV[0].scan(/127.0.0.[0-9]/).join
+puts ARGV[0].scan(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/).join(",")
