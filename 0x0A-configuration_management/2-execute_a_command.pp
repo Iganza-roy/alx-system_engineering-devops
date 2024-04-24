f64797cb0e1ff24d754940e7ec997d33f8e08b0d
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-24 06:21:12 -0700

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # kills a process named killmenow 
 
 exec { 'killmenow':
-  command   => 'pkill -9 killmenow',
-  path      => '/usr/bin:/bin',
-  onlyif    => 'pgrep killmenow',
-  provider  => shell,
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
 }
