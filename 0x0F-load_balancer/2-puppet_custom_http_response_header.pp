0a843b8f824d952f1b27368a344bcb8398ad967d
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-30 15:39:56 -0700

    Add 2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,5 @@
+# Automating custom HTTP header response
+exec { 'configure-nginx':
+  command  => 'apt-get -y update && apt-get -y install nginx && sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default && service nginx restart',
+  provider => shell,
+}
