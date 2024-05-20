6c4a9bd2ffa353a6c221a5e8a0b51bd1cbb8b9e7
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-20 10:56:50 -0700

    add 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""export data in the CSV format."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    name = employee.get("username")
+    to_do = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as f:
+        out = csv.writer(f, quoting=csv.QUOTE_ALL)
+        [out.writerow(
+            [user_id, employee, t.get("completed"), t.get("title")]
+         ) for t in to_do]
