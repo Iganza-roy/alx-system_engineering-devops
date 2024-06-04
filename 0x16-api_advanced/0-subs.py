ed9288bd8587fdd2cafc08a2b8f97988caee5883
Iganza-roy <iganzaroy55@gmail.com>
2024-06-04 14:39:22 -0700

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,19 @@ import requests
 def number_of_subscribers(subreddit):
     """a function that queries the Reddit API and returns the number of
     subscribers (not active users, total subscribers) for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Edge/125.0.2535.79 "
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 404:
+    if not subreddit or not isinstance(subreddit, str):
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {
+            "User-Agent": "Edge/125.0.2535.79 "
+        }
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 404:
+            return 0
+        results = response.json().get("data")
+        return results.get("subscribers")
+
+    except Exception:
+        return 0
