2ca3caca2d034686a4fe939077e41db455cef0df
Iganza-roy <iganzaroy55@gmail.com>
2024-06-04 14:34:13 -0700

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     subscribers (not active users, total subscribers) for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/91.0.864.48"
+        "User-Agent": "Edge/125.0.2535.79 "
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
 
